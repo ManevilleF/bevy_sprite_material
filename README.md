636f785f30566ac15afaf8762951f5303ad2cc19
Felix de Maneville <felix.maneville@gmail.com>
2022-01-14 13:18:51 +0100

    Fixed CI, added badges

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+<!-- cargo-sync-readme start -->
+
 # bevy_sprite_material
+
 Materials for sprites in Bevy
+
+[![workflow](https://github.com/ManevilleF/bevy_sprite_material/actions/workflows/rust.yaml/badge.svg)](https://github.com/ManevilleF/bevy_sprite_material/actions/workflows/rust.yaml)
+
+[![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+[![unsafe forbidden](https://img.shields.io/badge/unsafe-forbidden-success.svg)](https://github.com/rust-secure-code/safety-dance/)
+
+<!-- cargo-sync-readme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # bevy_sprite_material
+//!
+//! Materials for sprites in Bevy
+//!
+//! [![workflow](https://github.com/ManevilleF/bevy_sprite_material/actions/workflows/rust.yaml/badge.svg)](https://github.com/ManevilleF/bevy_sprite_material/actions/workflows/rust.yaml)
+//!
+//! [![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+//! [![unsafe forbidden](https://img.shields.io/badge/unsafe-forbidden-success.svg)](https://github.com/rust-secure-code/safety-dance/)
+
 // #![forbid(missing_docs)]
 #![forbid(unsafe_code)]
 #![warn(
